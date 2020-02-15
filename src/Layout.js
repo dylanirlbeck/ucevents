60c67765ac7212f9e123b968925089787f7f548b
dylanirlbeck <dylan@draftbit.com>
2020-02-15 16:24:12 -0600

    Setting up apollo client

---
+++ src/Layout.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+const ALL_EVENTS = gql`
+  {
+    getAllEvents {
+      name
+      description
+    }
+  }
+`;
+
+const Layout = () => {
+  const { loading, error, data } = useQuery(ALL_EVENTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.getAllEvents.map(({ name, description }) => (
+    <div key={name}>
+      <span className="font-bold">{name}</span>
+      <span>{description}</span>
+    </div>
+  ));
+};
+
+export default Layout;
