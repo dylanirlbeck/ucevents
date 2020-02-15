362df655a29807c24fc6590aa633885fa454878d
Dylan Irlbeck <dylan@draftbit.com>
2020-02-15 17:15:17 -0600

    Setting up apollo client (#5)
    * Setting up apollo client
    
    * Changing query name

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Layout from "./Layout.js";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+import { gql } from "apollo-boost";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const client = new ApolloClient({
+  uri: "http://34.69.193.74/",
+});
+
+client
+  .query({
+    query: gql`
+      {
+        getAllEvents {
+          name
+          description
+        }
+      }
+    `,
+  })
+  .then(result => console.log(result));
+
+//import Event from "./EventCard";
+/*function App() {*/
+//return (
+//<div className="App">
+//<header className="App-header">
+//<img src={logo} className="App-logo" alt="logo" />
+//<p>
+//Edit <code>src/App.js</code> and save to reload.
+//</p>
+//<a
+//className="App-link"
+//href="https://reactjs.org"
+//target="_blank"
+//rel="noopener noreferrer">
+//Learn React
+//</a>
+//</header>
+//</div>
+//);
+/*}*/
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <Layout />
+  </ApolloProvider>
+);
 
 export default App;
