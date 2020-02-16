d4850819a8f825d38291651368f34f50b0dcdb35
yikes <nodeadbatteries@gmail.com>
2020-02-16 09:05:52 -0600

    https heroku URI

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ApolloClient from "apollo-boost";
 import { gql } from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: "http://campus-outlook.herokuapp.com",
+  uri: "https://campus-outlook.herokuapp.com",
 });
 
 client
