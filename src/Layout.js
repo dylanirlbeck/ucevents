a96327b1ccaa5f7b49adf71ac5329ffd5e6578ea
yikes <nodeadbatteries@gmail.com>
2020-02-16 11:29:25 -0600

    Location display

--- src/Layout.js
+++ src/Layout.js
@@ -17,6 +17,7 @@ const ALL_EVENTS = gql`
       name
       tags
       image_url
+      location
       time {
         start
         end
@@ -32,6 +33,7 @@ const EVENTS_BY_TAG = gql`
       name
       tags
       image_url
+      location
       time {
         start
         end
@@ -80,13 +82,14 @@ const Layout = () => {
       <Navigation />
       <div className="container flex">
         <div className="px-20 mx-4 my-3 grid grid-cols-3 gap-4 row-gap-4">
-          {events.map(({ name, time, id, tags, image_url }) => (
+          {events.map(({ name, time, id, tags, location, image_url }) => (
             <Event
               key={id}
               name={name}
               date={new Date(1000 * time.start).toDateString()}
               tags={tags}
               image={image_url}
+              location={location}
               onClick={_ => {
                 openModal();
                 console.log("ONCLICKEVENT");
